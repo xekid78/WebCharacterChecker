ce49117b57f2cbc27b32b48263db5265226f6dfe
Makoto Tanabe <xekid78@outlook.com>
2018-05-27 12:10:32 +0900

    First upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+
+  var comment = document.getElementById('comment');
+  var label   = document.getElementById('label');
+
+  var LIMIT = 140;
+  var WARNING = 10;
+
+  label.innerHTML = LIMIT;
+
+  comment.addEventListener('keyup', function() {
+    var remaining = LIMIT - this.value.length;
+    label.innerHTML = remaining;
+    label.className = remaining < WARNING ? 'warning' : '';
+  });
+
+})();
